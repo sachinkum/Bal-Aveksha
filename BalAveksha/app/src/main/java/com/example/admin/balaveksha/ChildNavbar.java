cdf8a1f6f4e037210f77373220d570ce5c698bae
antrikshsaini <sainiantriksh6@gmail.com>
2017-03-23 03:48:46 +0530

    change in navigation drawer for child activity using fragments.

--- BalAveksha/app/src/main/java/com/example/admin/balaveksha/ChildNavbar.java
+++ BalAveksha/app/src/main/java/com/example/admin/balaveksha/ChildNavbar.java
@@ -1,5 +1,9 @@
 package com.example.admin.balaveksha;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +17,11 @@ public class ChildNavbar extends AppCompatActivity {
     private ActionBarDrawerToggle mToggle;
 
     private Toolbar mToolbar;
+
+    FragmentTransaction fragmentTransaction;
+    NavigationView navigationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,86 @@ public class ChildNavbar extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container,new HomeFragment());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle("Home");
+
+
+        navigationView=(NavigationView) findViewById(R.id.navigation_child);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new HomeFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Home");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+                        break;
+
+
+                    case R.id.account:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new AccountFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Account");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+
+                        break;
+
+                    case R.id.chatSession:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new SessionFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Session");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+                        break;
+
+                    case R.id.diagnosis:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new DiagnosisFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Diagnosis");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+                        break;
+
+                    case R.id.about:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new AboutFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("About");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+
+                        break;
+
+                    case R.id.logout:
+                        Intent intent = new Intent(getApplicationContext(), LoginScreen.class);
+                        startActivity(intent);
+
+
+                }
+
+
+
+                return true;
+            }
+        });
+
 
     }
 
