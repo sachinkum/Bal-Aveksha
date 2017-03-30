ddf50e50ade80cd5ee328cd8bb9ec467616c678d
stutivarshney <stutivarshney@hotmail.com>
2017-03-30 10:49:13 +0530

    Rest implementation extend

--- WebServer/Authentications/views.py
+++ WebServer/Authentications/views.py
@@ -1,27 +1,51 @@
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, render_to_response, redirect
-from django.contrib.auth.models import User
-from django.template import RequestContext
-from django.template import loader
-from django.urls import reverse_lazy
+from django.shortcuts import render
 from django.views import View
-from django.views import generic
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import authenticate, login
-from Authentications.forms import RegistrationForm
-from . import models
-from django.contrib.auth.models import User
-from Authentications.serializers import UserSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
-from Counsellee import urls
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+from Authentications.forms import RegistrationForm
 
 
 def homepageView(request):
+    '''
+    URL: '/'
+    Opens bal-aveksha homepage
+    '''
     return render(request, 'homepage/index.html')
+
+
+@api_view(['GET'])
+@authentication_classes((TokenAuthentication, JSONWebTokenAuthentication))
+@permission_classes((IsAuthenticated,))
+def setprofileView(request):
+    '''
+    URL: '/profile'
+    Opens Set User Profile Page if user is logged in
+             otherwise responds with HTTP_401_UNAUTHORIZED
+    '''
+    content = {
+        'user': request.user,  # `django.contrib.auth.User` instance.
+        'auth': request.auth  # None
+    }
+    if content['auth'] is not None:
+        return render(request, 'homepage/index.html', content)
+    else:
+        return Response(request, status.HTTP_401_UNAUTHORIZED)
+
+
+def signupView(request):
+    '''
+    URL: '/signup'
+    Opens Sign Up Page
+    '''
+    return render(request, 'homepage/index.html')
+
 
 def Index(request):
     return render(request, 'Home/index.html', {'user': request.user})
@@ -44,8 +68,8 @@ class RegisterUser(View):
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             email = form.cleaned_data['email']
-            first_name=form.cleaned_data['first_name']
-            last_name=form.cleaned_data['last_name']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
             user.set_password(password)
             user.save()
 
@@ -70,23 +94,23 @@ class RegisterUser(View):
 def register_success(request):
     return render(request, 'Authentications/Index.html')
 
-#
-# class UserList(APIView):
-#     """
-#     List all albums, or create a new album.
-#     """
-#     # def get(self, request, format=None):
-#     #     user = User.objects.all()
-#     #     serializer = UserSerializer(user, many=True)
-#     #     return Response(serializer.data)
-#
-#     def get(self, request, format = None):
-#         serializer=UserSerializer(data=request.data)
-#  #        serializer = UserSerializer(data=request.data)
-#   #       if serializer.is_valid():
-#              #serializer.save()
-#         user = authenticate(username="arjun1234", password="123456")
-#         if user is not None:
-#             return Response(user, status=status.HTTP_200_OK)
-#         else:
-#             return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+    #
+    # class UserList(APIView):
+    #     """
+    #     List all albums, or create a new album.
+    #     """
+    #     # def get(self, request, format=None):
+    #     #     user = User.objects.all()
+    #     #     serializer = UserSerializer(user, many=True)
+    #     #     return Response(serializer.data)
+    #
+    #     def get(self, request, format = None):
+    #         serializer=UserSerializer(data=request.data)
+    #  #        serializer = UserSerializer(data=request.data)
+    #   #       if serializer.is_valid():
+    #              #serializer.save()
+    #         user = authenticate(username="arjun1234", password="123456")
+    #         if user is not None:
+    #             return Response(user, status=status.HTTP_200_OK)
+    #         else:
+    #             return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
